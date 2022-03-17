b5101683a27dfbdbab033bbc5acc3cfaaee5b977
Alex Fernandes <alex.fernandes@mov.ai>
2022-03-17 19:08:05 +0000

    add install apt on entrypoint

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -22,41 +22,18 @@ ln -sf /proc/1/fd/1 ${APP_LOGS}/stdout
 
 [ -f ${MOVAI_HOME}/.welcome ] && cat ${MOVAI_HOME}/.welcome
 
-# Code from https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
-
-function compare_version() {
-
-    if [ "${1}" == "${2}" ]; then
-        return 0
-    fi
-
-    local IFS=.
-    local i ver1=(${1}) ver2=(${2})
-    # fill empty fields in ver1 with zeros
-    for ((i = ${#ver1[@]}; i < ${#ver2[@]}; i++)); do
-        ver1[i]=0
-    done
-
-    for ((i = 0; i < ${#ver1[@]}; i++)); do
-        if [[ -z ${ver2[i]} ]]; then
-            # fill empty fields in ver2 with zeros
-            ver2[i]=0
-        fi
-        if ((10#${ver1[i]} > 10#${ver2[i]})); then
-            return 1
-        fi
-        if ((10#${ver1[i]} < 10#${ver2[i]})); then
-            return 2
-        fi
-    done
-
-    return 0
-}
-
 # If we have updates on our update folder we do the update
-if [ $(ls ${APP_UPDATES}/movai-*.deb 2>/dev/null) ]; then
+if [ $(ls ${APP_UPDATES}/movai-*_latest_*.deb 2>/dev/null) ]; then
     # Run the update procedure and exit if everything goes well
     update.sh && exit
+fi
+
+# If we have packages on our env var we do install
+if [ -z "${APT_INSTALL_LIST}" ]; then
+    printf "APT Install list: %s\n" "${APT_INSTALL_LIST}"
+    apt-get update
+    DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install ${APT_INSTALL_LIST}
+    apt-get clean -y
 fi
 
 # Run command
--- files/update.sh
+++ files/update.sh
@@ -17,6 +17,37 @@
 # File: update.sh
 set -e
 
+# Code from https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
+
+function compare_version() {
+
+    if [ "${1}" == "${2}" ]; then
+        return 0
+    fi
+
+    local IFS=.
+    local i ver1=(${1}) ver2=(${2})
+    # fill empty fields in ver1 with zeros
+    for ((i = ${#ver1[@]}; i < ${#ver2[@]}; i++)); do
+        ver1[i]=0
+    done
+
+    for ((i = 0; i < ${#ver1[@]}; i++)); do
+        if [[ -z ${ver2[i]} ]]; then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]})); then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]})); then
+            return 2
+        fi
+    done
+
+    return 0
+}
+
 UPDATE_FILE=$(ls ${APP_UPDATES}/movai-*_latest_*.deb)
 
 # If there is no version file we create one
